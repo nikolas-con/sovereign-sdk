a4aff5a7ef2b8476a94c7b2f937ac47fcb3105f6
bkolad <blazejkolad@gmail.com>
2023-07-06 15:39:41 +0200

    Add rpc methods to the Evm module

--- full-node/sov-ethereum/src/lib.rs
+++ full-node/sov-ethereum/src/lib.rs
@@ -1,12 +1,6 @@
-use std::{convert::Infallible, hash::Hash};
-
-use ethers::{
-    types::{transaction::eip2718::TypedTransaction, Block, Bytes, FeeHistory, TxHash, U64},
-    utils::{
-        hex,
-        rlp::{self, Rlp},
-    },
-};
+use ethers::types::transaction::eip2718::TypedTransaction;
+use ethers::types::{Bytes, TxHash};
+use ethers::utils::rlp::Rlp;
 use jsonrpsee::RpcModule;
 
 pub struct Ethereum {}
@@ -25,26 +19,6 @@ fn register_rpc_methods(rpc: &mut RpcModule<Ethereum>) -> Result<(), jsonrpsee::
         unimplemented!()
     })?;
 
-    rpc.register_method("eth_blockNumber", |p, e| {
-        println!("eth_blockNumber");
-        Ok(unimplemented!())
-    })?;
-
-    rpc.register_method("eth_getTransactionByHash", |p, e| {
-        println!("eth_getTransactionByHash");
-        Ok(unimplemented!())
-    })?;
-
-    rpc.register_method("eth_getTransactionReceipt", |p, e| {
-        println!("eth_getTransactionReceipt");
-        Ok(unimplemented!())
-    })?;
-
-    rpc.register_method("get_block_number", |p, e| {
-        println!("get_block_number");
-        Ok(unimplemented!())
-    })?;
-
     rpc.register_method("eth_sendRawTransaction", |p, e| {
         println!("eth_sendRawTransaction");
         let data: Bytes = p.one().unwrap();
@@ -61,43 +35,6 @@ fn register_rpc_methods(rpc: &mut RpcModule<Ethereum>) -> Result<(), jsonrpsee::
 
         let h: TxHash = decoded_tx.sighash();
         Ok(h)
-    })?;
-
-    rpc.register_method("eth_getTransactionCount", |p, e| Ok(unimplemented!()))?;
-
-    // rpc.register_method("eth_chainId", |params, e| {
-    //    println!("eth_chainId");
-    //   Ok(Some(U64::from(1u64)))
-    //})?;
-
-    rpc.register_method("eth_getBlockByNumber", |params, e| {
-        println!("eth_getBlockByNumber");
-
-        let mut seq = params.sequence();
-        let b: &str = seq.next().unwrap();
-        let l: bool = seq.next().unwrap();
-
-        println!("{} {}", b, l);
-
-        let b = Block::<TxHash> {
-            base_fee_per_gas: Some(100.into()),
-            ..Default::default()
-        };
-
-        Ok(Some(b))
-    })?;
-
-    rpc.register_method("eth_feeHistory", |p, e| {
-        println!("eth_feeHistory");
-
-        let fh = FeeHistory {
-            base_fee_per_gas: Default::default(),
-            gas_used_ratio: Default::default(),
-            oldest_block: Default::default(),
-            reward: Default::default(),
-        };
-
-        Ok(fh)
     })?;
 
     Ok(())
--- module-system/module-implementations/sov-evm/src/query.rs
+++ module-system/module-implementations/sov-evm/src/query.rs
@@ -1,7 +1,9 @@
-use crate::Evm;
-use ethereum_types::U64;
+use ethereum_types::{Address, H256, U256, U64};
+use ethers_core::types::{Block, BlockId, FeeHistory, Transaction, TransactionReceipt, TxHash};
 use sov_modules_macros::rpc_gen;
 use sov_state::WorkingSet;
+
+use crate::Evm;
 
 #[rpc_gen(client, server, namespace = "eth")]
 impl<C: sov_modules_api::Context> Evm<C> {
@@ -9,5 +11,66 @@ impl<C: sov_modules_api::Context> Evm<C> {
     pub fn chain_id(&self, _working_set: &mut WorkingSet<C::Storage>) -> Option<U64> {
         println!("eth_chainId!");
         Some(U64::from(1u64))
+    }
+
+    #[rpc_method(name = "getBlockByNumber")]
+    pub fn get_block_by_number(
+        &self,
+        b: Option<String>,
+        l: Option<bool>,
+        _working_set: &mut WorkingSet<C::Storage>,
+    ) -> Option<Block<TxHash>> {
+        println!("eth_getBlockByNumber!");
+        println!("{:?} {:?}", b, l);
+
+        let b = Block::<TxHash> {
+            base_fee_per_gas: Some(100.into()),
+            ..Default::default()
+        };
+
+        Some(b)
+    }
+
+    #[rpc_method(name = "feeHistory")]
+    pub fn fee_history(&self, _working_set: &mut WorkingSet<C::Storage>) -> FeeHistory {
+        FeeHistory {
+            base_fee_per_gas: Default::default(),
+            gas_used_ratio: Default::default(),
+            oldest_block: Default::default(),
+            reward: Default::default(),
+        }
+    }
+
+    #[rpc_method(name = "blockNumber")]
+    pub fn block_number(&self, _working_set: &mut WorkingSet<C::Storage>) -> U256 {
+        unimplemented!()
+    }
+
+    #[rpc_method(name = "getTransactionByHash")]
+    pub fn get_transaction_by_hash(
+        &self,
+        _hash: H256,
+        _working_set: &mut WorkingSet<C::Storage>,
+    ) -> Option<Transaction> {
+        unimplemented!()
+    }
+
+    #[rpc_method(name = "getTransactionReceipt")]
+    pub fn get_transaction_receipt(
+        &self,
+        _hash: H256,
+        _working_set: &mut WorkingSet<C::Storage>,
+    ) -> Option<TransactionReceipt> {
+        unimplemented!()
+    }
+
+    #[rpc_method(name = "eth_getTransactionCount")]
+    pub fn get_transaction_count(
+        &self,
+        _address: Address,
+        _block_number: Option<BlockId>,
+        _working_set: &mut WorkingSet<C::Storage>,
+    ) -> Option<U256> {
+        unimplemented!()
     }
 }
